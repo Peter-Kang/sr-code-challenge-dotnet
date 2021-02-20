af85a245100e17278d7e308ce52774f0638973a7
Peter Kang <pxk7062@rit.edu>
2021-02-20 12:39:19 -0500

    Created inital data structure to hold the ReportingStructure information.

---
+++ code-challenge/Models/ReportingStructure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challenge.Models
+{
+    public class ReportingStructure
+    {
+
+        public int numberOfReports { get; set; }
+
+        public Employee employee { get; set; }
+    }
+}
