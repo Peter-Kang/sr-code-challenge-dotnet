2c6bacc62edb2bd434f6cd9244350ff93f35703f
Peter Kang <pxk7062@rit.edu>
2021-02-20 19:24:15 -0500

    Added the inital CompensationController class.

---
+++ code-challenge/Controllers/CompensationController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+using challenge.Services;
+using challenge.Models;
+
+namespace challenge.Controllers
+{
+    [Route("api/compensation")]
+    public class CompensationController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly ICompensationService _compensationService;
+
+        public CompensationController(ILogger<CompensationController> logger, ICompensationService compensationService) 
+        {
+            _logger = logger;
+            _compensationService = compensationService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompensation([FromBody] Compensation compensation)
+        {
+            if ( compensation.employeeID == null && compensation.employee.EmployeeId != null) 
+            {
+                compensation.employeeID = compensation.employee.EmployeeId;
+            }
+            _compensationService.Create(compensation);
+            return CreatedAtRoute("getCompensationById", new { id = compensation.employeeID }, compensation);
+        }
+
+        [HttpGet("{id}", Name = "getCompensationById")]
+        public IActionResult GetCompensationById(String id)
+        {
+            var compensation = _compensationService.GetById(id);
+            if (compensation == null)
+            {
+                return NotFound();
+            }
+            return Ok(compensation);
+        }
+
+
+    }
+}
