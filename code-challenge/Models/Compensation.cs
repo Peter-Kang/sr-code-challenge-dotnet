83300e096d85184df8c7af7cf84d272c879c7339
Peter Kang <pxk7062@rit.edu>
2021-02-20 19:26:58 -0500

    Added a key for dbcontext

--- code-challenge/Models/Compensation.cs
+++ code-challenge/Models/Compensation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace challenge.Models
 {
     public class Compensation
     {
+        [Key]
+        public String employeeID { get; set; }
         public Employee employee { get; set; }
         public double salary { get; set; }
         public DateTime effectiveDate { get; set; }
